edd85e9501476f3d9919c0e65b91309be97cfd77
Bryan Tan <bryan.tan@globaltium.com>
2020-07-01 14:53:33 +0800

    Initial commit

---
+++ src/JokeFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace bryanthw1020\ChuckNorrisJokes;
+
+class JokeFactory
+{
+    protected $jokes = [
+        "The First rule of Chuck Norris is: you do not talk about Chuck Norris.",
+        "Testing joke bruhhhh.",
+        "Chuck Norris' tears cure cancer. Too bad he has never cried."
+    ];
+
+    public function __construct(array $jokes = null)
+    {
+        if ($jokes) {
+            $this->jokes = $jokes;
+        }
+    }
+
+    public function getRandomJoke()
+    {
+        return $this->jokes[array_rand($this->jokes)];
+    }
+}
---
+++ tests/JokeFactoryTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace bryanthw1020\ChuckNorrisJokes\Tests;
+
+use bryanthw1020\ChuckNorrisJokes\JokeFactory;
+use PHPUnit\Framework\TestCase;
+
+class JokeFactoryTest extends TestCase
+{
+    /** @test */
+    public function it_returns_a_random_joke()
+    {
+        $jokes = new JokeFactory([
+            'This is a joke',
+        ]);
+        $joke = $jokes->getRandomJoke();
+
+        $this->assertSame('This is a joke', $joke);
+    }
+
+    /** @test */
+    public function it_returns_a_predefined_joke()
+    {
+        $chuckNorrisJokes = [
+            "The First rule of Chuck Norris is: you do not talk about Chuck Norris.",
+            "Chuck Norris does not wear a condom. Because there is no such thing as protection from Chuck Norris.",
+            "Chuck Norris' tears cure cancer. Too bad he has never cried."
+        ];
+        $jokes = new JokeFactory($chuckNorrisJokes);
+        $joke = $jokes->getRandomJoke();
+
+        $this->assertContains($joke, $chuckNorrisJokes);
+    }
+}
